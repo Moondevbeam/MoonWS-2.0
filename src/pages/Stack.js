052e6421e7fd7d56df7c0ff08dce796aa1254d9f
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-25 03:44:20 +0200

    deleted bug

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -5,31 +5,20 @@ import pdf from '../components/Images/RESUME.pdf'
 
 const Stack = () => {
     return(
-      <div className="phone">
-        <Navigation></Navigation>
+<div>
+    <Navigation></Navigation>
 {/* //start card */}
-<div className="dib br3 shadow-5 card out phone" style={{width:"18rem"}}>
-  <img src={Resume} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Do you want to hire me? Check out my Resume</p>
-    <div className="tc">
-    <button type="button" class="btn btn-warning"><a className="qbz black" href={pdf} download={pdf}>Download</a></button>
-    </div>
-    </div>
+  <div className="card out phone" style={{width:"18rem"}}>
+    <img src={Resume} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <p class="card-text">Do you want to hire me? Check out my Resume</p>
+      <div className="tc">
+      <button type="button" class="btn btn-warning"><a className="qbz black" href={pdf} download={pdf}>Download</a></button>
       </div>
+      </div>
+        </div>
 {/* // end card */}
-{/* //start card */}
-<div className=" dib br3 shadow-5 card out phone" style={{width:"18rem", marginTop:10}}>
-  <img src={Resume} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <p class="card-text">Do you want to hire me? Check out my Resume</p>
-    <div className="tc">
-    <button type="button" class="btn btn-warning"><a className="qbz black" href={pdf} download={pdf}>Download</a></button>
-    </div>
-    </div>
-      </div>
-{/* // end card */}
-      </div>
+</div>
     )
 }
 
