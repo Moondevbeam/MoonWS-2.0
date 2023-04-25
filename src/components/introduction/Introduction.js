577cd13bb6daa2a51a1d2227c4ec8bcd1873f0b4
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-25 03:05:17 +0200

    added new card in stack page

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -2,12 +2,13 @@ import React from "react";
 
 const Introduction = () =>{
     return(
-        <div className="sticky code black" style={{marginLeft:10}}>
+        <div className="phone sticky code black" style={{paddingLeft:10}}>
             <h1>Hello there! 👋🏻</h1>
             <h5>
         My name is Matteo (Moonbeam on the web).
-        I'm currently learning NodeJS, NextJS and React.</h5>
-    <h5>My dream is to become in a couple of years a Full Stack Developer.</h5>
+        I'm currently learning NodeJS and NextJS and React</h5>
+    <h5>I am a Front End developer with a passion for building dynamic and responsive web applications. 
+        My goal is to become a full-stack developer, and I am dedicated to learning the necessary skills to achieve this dream.</h5>
     </div>
     )
 }
--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -5,9 +5,10 @@ import pdf from '../components/Images/RESUME.pdf'
 
 const Stack = () => {
     return(
-      <div>
+      <div className="phone">
         <Navigation></Navigation>
-<div className="card out" style={{width:"18rem",display:"flex", justifyContent:"flex-start"}}>
+{/* //start card */}
+<div className="dib br3 shadow-5 card out phone" style={{width:"18rem"}}>
   <img src={Resume} class="card-img-top" alt="..."/>
   <div class="card-body">
     <p class="card-text">Do you want to hire me? Check out my Resume</p>
@@ -16,6 +17,18 @@ const Stack = () => {
     </div>
     </div>
       </div>
+{/* // end card */}
+{/* //start card */}
+<div className=" dib br3 shadow-5 card out phone" style={{width:"18rem", marginTop:10}}>
+  <img src={Resume} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <p class="card-text">Do you want to hire me? Check out my Resume</p>
+    <div className="tc">
+    <button type="button" class="btn btn-warning"><a className="qbz black" href={pdf} download={pdf}>Download</a></button>
+    </div>
+    </div>
+      </div>
+{/* // end card */}
       </div>
     )
 }
