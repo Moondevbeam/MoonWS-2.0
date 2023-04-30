993490d6de4ebfc02226fac2adedd771dd5e66c4
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-30 11:00:12 +0200

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React,{Component} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Introduction from './pages/Introduction';
 import Stack from './pages/Stack';
 class App extends Component{
   render(){
     return(
-    <Router basename="/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Introduction />} />
         <Route path="/stack" element={<Stack />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
   }
