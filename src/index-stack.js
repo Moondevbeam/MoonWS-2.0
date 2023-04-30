79de120eab6e4e1a35951a35ae45c14073c2e1a3
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-30 10:14:33 +0200

    making new root for resume page

--- src/index-stack.js
+++ src/index-stack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Stack from './pages/Stack';
 
-const stackRoot = document.getElementById('index-stack');
+const stackRoot = document.getElementById('stack-root');
 ReactDOM.render(
   <React.StrictMode>
     <Stack />
