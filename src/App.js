41ccdd15a5fe63266c1c7dbbd774237ca12e87c1
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-30 09:16:54 +0200

    fixing route bug

--- src/App.js
+++ src/App.js
@@ -2,25 +2,14 @@ import React,{Component} from 'react';
 import Navigation from './components/navigation/Navigation';
 import './App.css';
 import Introduction from './components/introduction/Introduction';
-import Stack from './pages/Stack'
 class App extends Component{
   render(){
-      const path = window.location.pathname;
-      if (path === "/stack") {
-        return (
-          <div>
-            <Navigation />
-        <Stack />;
-        </div>
-        )
-      } else {
-        return (
-          <div>
-            <Navigation />
-        <Introduction />;
-        </div>
-        )
-      }
+    return(
+      <div className='App'>
+        <Navigation/>
+        <Introduction/>
+      </div>
+    )
   }
 }
 export default App;
