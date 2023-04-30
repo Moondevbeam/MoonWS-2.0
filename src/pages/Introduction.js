4d55f53055f3c73b42d31a1941cfe999a04cc40e
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-30 13:04:45 +0200

    fixed typo in introduction

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -8,7 +8,7 @@ const Introduction = () =>{
             <h1>Hello there! 👋🏻</h1>
             <h5>
         My name is Matteo (Moonbeam on the web).
-        I'm currently learning NodeJS and NextJS and React</h5>
+        I'm currently learning NodeJS, NextJS and React</h5>
     <h5>I am a Front End developer with a passion for building dynamic and responsive web applications. 
         My goal is to become a full-stack developer, and I am dedicated to learning the necessary skills to achieve this dream.</h5>
     </div>
