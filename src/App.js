a74d250a84bdcddc9f9f735fc37bc981a8af28ef
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-30 10:54:55 +0200

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React,{Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Introduction from './pages/Introduction';
+import Stack from './pages/Stack';
 class App extends Component{
   render(){
     return(
-      <div className='App'>
-          <Introduction/>
-      </div>
-    )
-  }
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Introduction />} />
+        <Route path="/stack" element={<Stack />} />
+      </Routes>
+    </Router>
+  );
 }
+  }
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,29 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Stack from './pages/Stack';
 // eslint-disable-next-line
 import tachyons from 'tachyons';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<App/>,
-  },
-  {
-    path: "stack",
-    element:<Stack/>,
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
 
