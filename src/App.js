a5d9ec8521ea8041ed6d7b3738ab2f96b81c1177
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-04-30 06:53:23 +0200

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React,{Component} from 'react';
 import Navigation from './components/navigation/Navigation';
 import './App.css';
-import Darkmode from './components/darkmode/Darkmode';
 import Introduction from './components/introduction/Introduction';
 class App extends Component{
   render(){
     return(
       <div className='App'>
-        <Darkmode/>
         <Navigation/>
         <Introduction/>
       </div>
