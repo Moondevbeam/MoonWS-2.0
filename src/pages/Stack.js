f07e0e9ca3c1a78dfdecbcde425dc13dad3a8371
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-05-23 00:53:37 +0200

    add flexbox feature for future upgrades

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -4,22 +4,29 @@ import Resume from '../components/Images/RESUME.jpg'
 import pdf from '../components/Images/RESUME.pdf'
 
 const Stack = () => {
-    return(
-<div>
-    <Navigation></Navigation>
-{/* //start card */}
-  <div className="card out phone" style={{width:"18rem"}}>
-    <img src={Resume} class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <p class="card-text">Do you want to hire me? Check out my Resume</p>
-      <div className="tc">
-      <button type="button" class="btn btn-warning"><a className="qbz black" href={pdf} download={pdf}>Download</a></button>
-      </div>
-      </div>
+  return (
+    <div>
+        <Navigation></Navigation>
+        {/* start card container */}
+        <div className="flex items-center">
+            {/* //start card */}
+            <div className="card out phone mr3" style={{width:"18rem"}}>
+                <img src={Resume} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Do you want to hire me? Check out my Resume</p>
+                    <div className="tc">
+                        <button type="button" class="btn btn-warning">
+                            <a className="qbz black" href={pdf} download={pdf}>Download</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/* // end card */}
+            {/* Add more cards here */}
         </div>
-{/* // end card */}
-</div>
-    )
+        {/* end card container */}
+    </div>
+)
 }
 
 export default Stack;
