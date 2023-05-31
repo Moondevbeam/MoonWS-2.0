e6bd8502676249268dd6a98c03c6ee24d993e38c
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-05-31 22:34:54 +0200

    changed Navigation UX

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ imgSrc, cardTitle, onOpen }) => {
   return (
-    <div className="card out phone mr3 pointer w5 bg-purple" style={{ border: "none", display: "grid", gridTemplateRows: "auto 1fr", transition: "box-shadow 0.3s" }} onClick={onOpen}>
+    <div className="card out phone mr3 pointer w5 bg-purple" style={{ border: "none", display: "grid", gridTemplateRows: "auto 1fr", marginBottom:"10px" ,transition: "box-shadow 0.3s" }} onClick={onOpen}>
       <img src={imgSrc} className="card-img-top" alt="..." style={{ borderRadius: "0", width: "100%" }} />
       <div className="card-body" style={{ display: "flex", alignItems: "center" }}>
         <h5 className="tc f3 code card-title">{cardTitle}</h5>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,42 +4,47 @@ import Darkmode from "../darkmode/Darkmode";
 import "./Navigation.css";
 import { Link, useLocation } from "react-router-dom";
 
-const MenuLink = ({ to, iconClass, external, children }) => {
-    if (external) {
-      return (
-        <a className="pointer pa3 f3 black" href={to}><i class={iconClass}></i></a>
-      );
-    } else {
-      return (
-        <Link className="pointer pa3 f3 black" to={to}><i class={iconClass}></i></Link>
-      );
-    }
-  }
-  
-  const Navigation = () => {
-    const location = useLocation();
-    const commonLinks = [
-      { to: "https://www.linkedin.com/in/moonbeam-dev/", iconClass: "bi bi-linkedin", external: true },
-      { to: "https://github.com/Moondevbeam", iconClass: "bi bi-github", external: true },
-      { to: location.pathname === '/' ? "/projects" : "/", iconClass: location.pathname === '/' ? "bi bi-file-earmark-code" : "bi bi-house-heart-fill", external: false }
-    ];
-  
+const MenuLink = ({ to, iconClass, external, onClick, children }) => {
+  if (external) {
     return (
-      <div className="pa3 sticky dt ph5-ns black">
-        <Link to="/" className="dtc v-mid link w-25">
-          <img src={logo} className="dib br-100" alt="Logo" style={{ width: 48, height: 48 }} />
-        </Link>
-        <div style={{ display: "flex", justifyContent: "right" }}>
-          {commonLinks.map((link, index) => (
-            <MenuLink key={index} {...link} />
-          ))}
-          <button onClick={Darkmode} className="pointer pa3 f3 black" style={{ background: 'transparent', border: 'none' }}>
-            <i class="bi bi-moon-stars-fill"></i>
-          </button>
-        </div>
-      </div>
-    )
+      <a className="pointer pa3 f3 black" href={to}><i className={iconClass}></i></a>
+    );
+  } else {
+    return (
+      <Link className="pointer pa3 f3 black" to={to} onClick={onClick}><i className={iconClass}></i></Link>
+    );
   }
-  
-  export default Navigation;
-  
+}
+
+const Navigation = () => {
+  const location = useLocation();
+  const commonLinks = [
+    { to: "https://www.linkedin.com/in/moonbeam-dev/", iconClass: "bi bi-linkedin", external: true },
+    { to: "https://github.com/Moondevbeam", iconClass: "bi bi-github", external: true },
+  ];
+
+  const homeAndProjectsLinks = [
+    { to: location.pathname === '/' ? "/projects" : "/", iconClass: location.pathname === '/' ? "bi bi-file-earmark-code" : "bi bi-house-heart-fill", external: false },
+    { to: "#", iconClass: "bi bi-moon-stars-fill", external: false, onClick: Darkmode }
+  ];
+
+  return (
+    <div className="pa3 sticky dt ph5-ns black" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+      <div style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+        {homeAndProjectsLinks.map((link, index) => (
+          <MenuLink key={index} {...link} />
+        ))}
+      </div>
+      <div style={{ display: "flex", justifyContent: "right", alignItems: "center" }}>
+        {commonLinks.map((link, index) => (
+          <MenuLink key={index} {...link} />
+        ))}
+        <Link to="/" className="dtc v-mid link w-25" style={{ paddingLeft: '20px' }}>
+          <img src={logo} className="dib br-100" alt="Logo" style={{ width: 100 }} />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation;
