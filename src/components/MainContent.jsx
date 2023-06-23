48672106a5fe93b18858c3044f559845020795a5
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-06-23 19:37:51 +0200

    added main content without styling

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <div>
+        <h1>Hi! This is my blog!</h1>
+        <h2>I will be documenting my coding journey once or twice a week.</h2>
+    </div>
+  )
+}
+
+export default MainContent
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Introduction from '../components/Introduction'
 import Thanks from '../components/Thanks'
+import MainContent from '../components/MainContent'
 
 function Blog() {
   return (
     <div>
         <Navbar/>
         <Introduction/>
+        <MainContent/>
         <Thanks/>
         <Footer/>
     </div>
