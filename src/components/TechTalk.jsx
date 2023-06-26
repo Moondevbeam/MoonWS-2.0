a0e1e9d6cffc3d0ae054ec6298958a3218ccf7e9
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-06-26 22:55:49 +0200

    added a new component in blog page

---
+++ src/components/TechTalk.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const TechTalk = () => {
+  return (
+    <div className="mx-4 mb-8 max-w-prose sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h1 className="text-2xl sm:text-3xl leading-9 font-bold">Introducing Myself: Matthew</h1>
+        <p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+          Published on June 26, 2023
+        </p>
+      </div>
+      <div className="px-4 py-5 sm:p-6">
+        <p className="text-base sm:text-lg leading-7">
+          Hello, everyone! My name is Matthew, and I hail from the beautiful country of Italy. After completing high school, I discovered my passion for various tech-related fields, including coding, video games, hardware, and computers.
+        </p>
+        <p className="mt-4 text-base sm:text-lg leading-7">
+          My journey in the world of coding began as a self-taught enthusiast, delving into online resources and tutorials. Eager to expand my knowledge and skills, I enrolled in a web development course on Udemy, which provided a solid foundation for my learning. Since then, I have continuously honed my abilities and explored new concepts, resulting in the development of a diverse portfolio and a range of projects showcased on my website.
+        </p>
+        <p className="mt-4 text-base sm:text-lg leading-7">
+          Currently, my specialization lies in Front-End Development, with a particular focus on utilizing React. I'm also in the process of expanding my skill set by learning Next.js, a framework that enables server-rendered React applications. Embracing modern web technologies and staying up to date with industry trends is an essential part of my professional growth.
+        </p>
+        <p className="mt-4 text-base sm:text-lg leading-7">
+          As I embark on my career journey, my immediate goal is to secure a position in the field of web development, preferably in a fully remote capacity, focusing on Front-End development. Looking further ahead, I aspire to become an SEO specialist, combining my technical skills with an in-depth understanding of search engine optimization. Ultimately, my aim is to evolve into a seasoned Front-End Developer, taking on challenging projects and contributing to the ever-evolving web development landscape.
+        </p>
+        <p className="mt-4 text-base sm:text-lg leading-7">
+          Thank you for taking the time to get to know me, and I look forward to connecting with like-minded professionals and potential collaborators in this exciting industry.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TechTalk;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Introduction from '../components/Introduction'
 import Thanks from '../components/Thanks'
+import TechTalk from '../components/TechTalk'
 
 function Blog() {
   return (
     <div>
         <Navbar/>
         <Introduction/>
+        <TechTalk/>
         <Thanks/>
         <Footer/>
     </div>
