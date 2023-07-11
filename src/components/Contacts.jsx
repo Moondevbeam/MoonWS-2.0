905c91ee19c06187fc2322a548fb30784b005f02
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-07-11 21:58:26 +0200

    added links in contacts section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -9,14 +9,18 @@ const Contacts = () => {
       <div className="mt-10 flex flex-col items-center md:flex-row md:justify-start">
         <div className="w-full flex flex-col items-center mb-16 md:mb-0 md:mr-8">
           <div className="flex-grow hover:scale-125 cursor-pointer">
+            <a href="https://www.linkedin.com/in/moonbeam-dev/" aria-label='LinkedIn Account'>
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn" className="mb-2 mx-auto h-16 w-16"/>
-          <h3 className="text-sm text-center font-extrabold"><a href="https://www.linkedin.com/in/moonbeam-dev/" aria-label='LinkedIn Account'>LinkedIn</a></h3>
+          <h3 className="text-sm text-center font-extrabold">LinkedIn</h3>
+          </a>
           </div>
         </div>
         <div className="w-full flex flex-col items-center mb-16 md:mb-0 md:mr-8">
           <div className="flex-grow hover:scale-125 cursor-pointer">
+            <a href="https://github.com/Moondevbeam" aria-label='Github Account'>
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="Github" className="mb-2 mx-auto h-16 w-16"/>
-          <h3 className="text-sm text-center font-extrabold"><a href="https://github.com/Moondevbeam" aria-label='Github Account'>Github</a></h3>
+          <h3 className="text-sm text-center font-extrabold">Github</h3>
+          </a>
             <div className="flex items-center justify-center mt-4">
             </div>
           </div>
