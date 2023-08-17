ba1361564f66336ffceef7023861cece02a4d1da
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-08-17 23:26:30 +0200

    removed thanks section in projects and moved footer into proper position

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -26,10 +26,10 @@ const CardSlider = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 my-4 mx-4">
       {projects.map((project) => (
         <Link key={project.id} to={`/projects/${project.id}`}>
-          <div className="bg-white rounded-md shadow-md cursor-pointer">
+          <div className="bg-[#8000ff] bg-opacity-[0.7] rounded-md shadow-md cursor-pointer">
             <img src={project.imageURL} alt={project.title} className="w-full h-64 object-cover mb-2 rounded-t" />
             <div className="p-4">
-              <h3 className="text-md text-center mb-1 font-semibold text-gray-800">{project.title}</h3>
+              <h3 className="text-md text-white text-center mb-1 font-semibold text-gray-800">{project.title}</h3>
             </div>
           </div>
         </Link>
