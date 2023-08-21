b2d3616d34f07427ca272071ea9a93bd88c97e63
Moonbeam <122624659+Moondevbeam@users.noreply.github.com>
2023-08-22 01:23:35 +0200

    changed grid columns from 4 to 3

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -24,11 +24,11 @@ const CardSlider = () => {
 
   return (
     <div className='min-h-screen mt-8 md:mb-8 mb-16'>
-    <div className="grid grid-cols-1 xl:grid-cols-5 md:grid-cols-2 lg:grid-cols-4 gap-8 mx-4">
+    <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 lg:grid-cols-2 gap-8 md:mx-8 mx-4">
       {projects.map((project) => (
         <Link key={project.id} to={`/projects/${project.id}`}>
           <div className="bg-[#8000ff] bg-opacity-[0.7] rounded-md hover:shadow-md hover:shadow-purple-700 cursor-pointer transform hover:scale-105">
-            <img src={project.imageURL} alt={project.title} className="w-full h-64 object-cover mb-2 rounded-t" />
+            <img src={project.imageURL} alt={project.title} className="w-full h-80 object-cover mb-2 rounded-t" />
             <div className="p-4">
               <h3 className="text-md text-white text-center mb-1 font-semibold text-gray-800">{project.title}</h3>
             </div>
